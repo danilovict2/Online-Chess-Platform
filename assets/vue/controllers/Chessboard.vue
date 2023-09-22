e2adffb159f41df192144dcdb17ea42945d7117d
oiahwdoihawoidh <labudovomudo0@gmail.com>
2023-09-22 13:02:23 +0200

    Add Board

---
+++ assets/vue/controllers/Chessboard.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="chessboard">
+        <div 
+            v-for="(square, i) in board" :key="i"
+            class="tile" :class="square % 2 == 0 ? 'black-tile' : 'white-tile'"
+        ></div>
+    </div>
+</template>
+
+<script setup>
+
+const boardDimension = 8;
+
+let board = [];
+for (let j = boardDimension - 1; j >= 0; --j) {
+    for (let i = 0; i < boardDimension; ++i) {
+        board.push(i + j);
+    }
+}
+
+</script>
+
+<style scoped>
+#chessboard {
+    display: grid;
+    grid-template-columns: repeat(8, 100px);
+    grid-template-rows: repeat(8, 100px);
+    width: 800px;
+    height: 800px;
+}
+
+.tile {
+    width: 100px;
+    height: 100px;
+}
+
+.white-tile {
+    background-color: #ebecd0;
+}
+
+.black-tile {
+    background-color: #779556;
+}
+</style>
