68c8613cbd58d762a7cb9f9c7f1aaed601410b19
danilovict2 <danilosindric@gmail.com>
2023-10-06 22:27:03 +0200

    Implement Bishop Movement

--- assets/vue/composables/referee.js
+++ assets/vue/composables/referee.js
@@ -1,14 +1,17 @@
+import BishopReferee from "./referees/BishopReferee.js";
 import KnightReferee from "./referees/KnightReferee.js";
 import PawnReferee from "./referees/PawnReferee.js";
 
 export default function useReferee() {
     function createRefereeForType(type) {
-        switch (type){
+        switch (type) {
             case 'Pawn':
                 return new PawnReferee();
             case 'Knight':
                 return new KnightReferee();
-            default: 
+            case 'Bishop':
+                return new BishopReferee();
+            default:
                 throw new Error('Invalid type');
         }
     }
