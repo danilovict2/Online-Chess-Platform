f6bf9e2bdc6bf2eda1c40a98769aacdadd73d9fa
danilovict2 <danilosindric@gmail.com>
2023-11-01 22:37:54 +0100

    Imlpement login

--- assets/app.js
+++ assets/app.js
@@ -5,8 +5,3 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 
 registerVueControllerComponents(require.context('./vue/controllers', true, /\.vue$/));
-
-function showPassword() {
-    const password = document.getElementById("password");
-    password.type = password.type === 'password' ? "text" : "password";
-}
