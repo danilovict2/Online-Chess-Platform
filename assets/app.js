b0f8a5647a8f83928cc1665ea44512c19d78c6b5
danilovict2 <danilosindric@gmail.com>
2023-11-01 22:33:39 +0100

    Implement login

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,12 @@
 import { registerVueControllerComponents } from '@symfony/ux-vue';
 import './bootstrap.js';
 import './styles/app.scss';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/js/all.js';
 
 registerVueControllerComponents(require.context('./vue/controllers', true, /\.vue$/));
+
+function showPassword() {
+    const password = document.getElementById("password");
+    password.type = password.type === 'password' ? "text" : "password";
+}
