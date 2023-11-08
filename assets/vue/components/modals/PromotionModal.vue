d8d665e7c2954d3723058a9a60d23f2cb3614c66
danilovict2 <danilosindric@gmail.com>
2023-11-08 20:14:37 +0100

    Bug fixes

--- assets/vue/components/modals/PromotionModal.vue
+++ assets/vue/components/modals/PromotionModal.vue
@@ -1,17 +1,20 @@
 <template>
     <div class="custom-modal">
         <div class="body">
-            <img :src="`images/rook_${team}.png`" @click="promoteTo('Rook')">
-            <img :src="`images/bishop_${team}.png`" @click="promoteTo('Bishop')">
-            <img :src="`images/knight_${team}.png`" @click="promoteTo('Knight')">
-            <img :src="`images/queen_${team}.png`" @click="promoteTo('Queen')">
+            <img :src="`/images/rook_${team}.png`" @click="promoteTo('Rook')">
+            <img :src="`/images/bishop_${team}.png`" @click="promoteTo('Bishop')">
+            <img :src="`/images/knight_${team}.png`" @click="promoteTo('Knight')">
+            <img :src="`/images/queen_${team}.png`" @click="promoteTo('Queen')">
         </div>
     </div>
 </template>
 
 <script setup>
 const { team } = defineProps({
-    team: String
+    team: {
+        type: String,
+        default: 'w'
+    }
 });
 const emit = defineEmits(['promoteTo']);
 
