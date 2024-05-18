3e506f933dc9c4ecea664d8d03697e5625420cfd
Danilo <danilosindric@gmail.com>
2024-05-18 20:46:11 +0200

    Add possible En Passant Targets to fen string

--- assets/vue/services/compress.js
+++ assets/vue/services/compress.js
@@ -1,8 +1,10 @@
 import { BOARD_DIMENSION } from "../common/constants";
+import { getKingOfTeam } from "../common/helpers";
 import { board } from "../stores/board";
 
 export function compressPieceState(pieceState) {
-    let compressedPieceState = '';
+    let fen = '';
+    let possibleEnPassantTargets = '';
     let emptyCount = 0;
 
     for (let j = BOARD_DIMENSION; j >= 1; --j) {
@@ -11,29 +13,55 @@ export function compressPieceState(pieceState) {
 
             if (foundPiece) {
                 if (emptyCount > 0) {
-                    compressedPieceState += emptyCount.toString();
+                    fen += emptyCount.toString();
                     emptyCount = 0;
                 }
 
                 const pieceTypeSymbol = foundPiece.type === 'Knight' ? 'n' : foundPiece.type[0].toLowerCase();
-                compressedPieceState += foundPiece.team === 'w' ? pieceTypeSymbol.toUpperCase() : pieceTypeSymbol;
+                fen += foundPiece.team === 'w' ? pieceTypeSymbol.toUpperCase() : pieceTypeSymbol;
+
+                const subs = (foundPiece.team === 'w') ? 1 : -1;
+                possibleEnPassantTargets += foundPiece.enPassant ? ' ' + String.fromCharCode(97 + (i-1)) + (j-subs) : '';
             } else {
                 emptyCount++;
             }
         }
 
         if (emptyCount > 0) {
-            compressedPieceState += emptyCount.toString();
+            fen += emptyCount.toString();
             emptyCount = 0;
         }
 
         if (j > 1) {
-            compressedPieceState += '/';
+            fen += '/';
         }
     }
 
     const nextToMove = ` ${board.turn % 2 === 0 ? 'b' : 'w'}`;
- 
+    const whiteCastlingRights = getCastlingRights('w', pieceState);
+    const blackCastlingRights = getCastlingRights('b', pieceState);
+    const castlingFenPart = (whiteCastlingRights === blackCastlingRights) ? '-' :  whiteCastlingRights + blackCastlingRights;
+    possibleEnPassantTargets = possibleEnPassantTargets.length ? possibleEnPassantTargets : ' -';
 
-    return compressedPieceState;
+    fen += nextToMove + ' ' + castlingFenPart + possibleEnPassantTargets;
+
+    return fen;
+}
+
+function getCastlingRights(color, pieceState) {
+    if (getKingOfTeam(color).hasMoved)return '';
+    
+    if (color === 'w') {
+        let castlingRights = '';
+        if (!pieceState.get('1-1').hasMoved)castlingRights += 'K';
+        if (!pieceState.get('8-1').hasMoved)castlingRights += 'Q';
+        return castlingRights.length ? castlingRights : '-';
+    } else {
+        let castlingRights = '';
+        if (!pieceState.get('1-8').hasMoved)castlingRights += 'q';
+        if (!pieceState.get('8-8').hasMoved)castlingRights += 'k';
+        return castlingRights.length ? castlingRights : '-';
+    }
+
+    return '';
 }
