409dd74dc319851d4b2b02d09068ab23a675e46f
Danilo <danilosindric@gmail.com>
2024-06-13 22:12:17 +0200

    Fix skill level errors

--- src/Controller/EngineController.php
+++ src/Controller/EngineController.php
@@ -7,6 +7,7 @@ use App\StockfishService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class EngineController extends AbstractController
@@ -19,5 +20,11 @@ class EngineController extends AbstractController
         $bestMove = $stockfish->getBestMove($game->getEngine()->getSkillLevel(), $game->getFen());
 
         return new JsonResponse($bestMove);
+    }
+
+    #[Route('test')]
+    public function test(StockfishService $stockfish): Response
+    {
+        dd($stockfish->getBestMove(20, 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1'));
     }
 }
